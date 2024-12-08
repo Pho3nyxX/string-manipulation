dd1539953e663a793ccf8c22f177076d0c508e2b
Pho3nyxX <60277772+Pho3nyxX@users.noreply.github.com>
2024-12-07 20:07:06 -0500

    acronym works

---
+++ strings/acronym.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Acronym
+{
+    public string Splitter(string phrase){
+        string[] splittingPhrase = phrase.Split(' ');
+
+        string acronym = "";
+
+        foreach(string split in splittingPhrase){
+            acronym = acronym + split[0];
+        }
+        return acronym;
+    }
+
+    public static void Main(string[] args)
+    {
+        Acronym acro = new Acronym();
+        string givenPhrase = "As Soon As Possible";
+
+        string result = acro.Splitter(givenPhrase);
+        Console.WriteLine (result);
+    }
+}
